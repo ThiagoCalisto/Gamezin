cf29ca39e0dbb56f2ebe898f8c080fa207d3a4d0
ThiagoCalisto <warlocalgames@gmail.com>
2022-04-06 02:11:46 -0300

    camera dinamica adicionada e colisao tbm

--- level.py
+++ level.py
@@ -11,8 +11,8 @@ class level:
         self.display_surface = pygame.display.get_surface()
 
         #sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.visible_sprites = YsortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
 
     
         self.criar_map()
@@ -26,16 +26,36 @@ class level:
                 if col == 'x':
                     #como ficaria o jogo se o snake fizesse >>>>> 
                     #self.player = Player((x,y),[self.visible_sprites])
-                    Tile((x,y),[self.visible_sprites])
+                    Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
                 if col == 'p':
                     #como ficaria o jogo se o snake fizesse >>>>> 
                     #Tile((x,y),[self.visible_sprites])
-                    self.player =  Player((x,y),[self.visible_sprites])
+                    self.player =  Player((x,y),[self.visible_sprites],self.obstacle_sprites)
                 
 
     def run (self):
 
         #Atualizar e desenhar o joguin
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+
+class YsortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+            #setup geral
+            super().__init__()
+            self.display_surface = pygame.display.get_surface()
+            self.half_width = self.display_surface.get_size()[0] // 2
+            self.half_height = self.display_surface.get_size()[1] // 2
+            self.offset = pygame.math.Vector2(100,200)
+
+    def custom_draw(self,player):
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        #for sprite in self.sprites():
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
